e2747e8677e474baf0f3897c7b85e5f5cf636361
François Geremie <francois.geremie@student.umons.ac.be>
2022-01-05 00:11:22 +0100

    3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from random import randint
+from copy import deepcopy
 import numpy as np
 
 
@@ -16,46 +17,49 @@ class Node:
     def updatePossibleActions(self):
         emptySlots = []
         for i in range(self.size):
-            if len(emptySlots) > 4:
+            if len(emptySlots) > 3:
                 break
-            if self.board[i] is None:
+            if self.board[i] == 0:
                 emptySlots.append(i)
         random.shuffle(emptySlots)
         print(emptySlots)
         self.possibleActions = emptySlots
 
-    def checkStatus(self):
-        if self.board[0] == 0:
-            return 0
-        elif self.board[0] == 1:
-            return 1
-        else:
-            return -1
-
-    def select(self):
-        listUCB = [x.numberOfWins for x in self.childArray]
+    def select(self, c, t):
+        listUCB = [((x.numberOfWins/x.numberOfVisits)+c*np.sqrt(np.log(t)/x.numberOfVisits)) for x in self.childArray]
+        print(listUCB)
         return self.childArray[np.argmax(listUCB)]
 
     def expand(self):
         action = self.possibleActions.pop()
-        print(action)
-        #print(self.board)
-        childNode = Node(self.board, parent=self)
-        #print(self.board)
+        childNode = Node(deepcopy(self.board), parent=self) #si on met direct self.board, l'enfant et le parent auront tous les 2 la même array avec 2 références différentes. La modification dans un des objets impactera l'array et cette modification sera visible dans l'autre objet
         childNode.board[action] = 1
-        #print(childNode, childNode.parent, self)
-        #print(childNode.board, childNode.parent.board, self.board)
         self.childArray.append(childNode)
         return childNode #comment le return sait que childNode est dans self ? Il ne retourne pas juste un objet ?
 
     def simulate(self):
-        self.board[randint(0, 8)] = 9
+        simulation = deepcopy(self.board)
+        toFill = [i for i, item in enumerate(self.board) if item == 0]
+        while toFill:
+            simulation[toFill.pop()] = randint(3,4)
+        return determineWinner(simulation)
 
-    def backPropagate(self):
+    def backPropagate(self, winner):
         self.numberOfVisits += 1
+        if winner == 1:
+            self.numberOfWins += 1
         print(self.numberOfVisits)
         if self.parent:
-            self.parent.backPropagate()
+            self.parent.backPropagate(winner)
+
+
+def determineWinner(simulation):
+    c1 = simulation.count(3)
+    c2 = simulation.count(4)
+    if c1 > c2:
+        return 1
+    else:
+        return 2
 
 
 def alternate():
@@ -68,25 +72,25 @@ if __name__ == '__main__':
     board = [0] * 9
     root = Node(board)
 
-    t = 2
-    for i in range(t):
+    time = 2
+    t = 0
+    for i in range(time):
+        t += 1
         node = root
         while node.childArray:
-            print("on descend")
-            node = node.select()
+            node = node.select(np.sqrt(2), t)
         print("selected " + str(node.board))
         node.updatePossibleActions()
         while node.possibleActions:
             node.expand()
-            for x in node.childArray:
-                print(x.board)
-        #print(len(node.childArray))
         a = randint(0, len(node.childArray)-1)
         print(a)
         child = node.childArray[a]
-        child.simulate()
-
-        child.backPropagate()
+        w = child.simulate()
+        for x in node.childArray:
+            print(x.board)
+        print("winner : " + str(w))
+        child.backPropagate(w)
 
 
 
