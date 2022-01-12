0c4d9e7520195c8fc7592989885aab5a996cdfd2
François Geremie <francois.geremie@student.umons.ac.be>
2022-01-12 01:10:29 +0100

    12

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from random import randint
 from copy import deepcopy
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
 class Node:
-    def __init__(self, board, player=None, parent=None, move=None):
+    def __init__(self, board, rootPlayer, player=None, parent=None, move=None):
         self.board = board
         self.player = player
+        self.rootPlayer = rootPlayer
         self.parent = parent
         self.childArray = []
         self.numberOfVisits = 0
@@ -27,19 +29,21 @@ class Node:
 
     def expand(self):
         action = self.possibleActions.pop()
-        childNode = Node(deepcopy(self.board), player=3-self.player, parent=self, move=action)  # si on met simplement self.board, l'enfant et le parent auront tous les 2 la même array avec 2 références différentes. La modification dans un des objets impactera l'array et cette modification sera visible dans l'autre objet
-        childNode.board[action] = self.player
+        childNode = Node(deepcopy(self.board), rootPlayer=self.rootPlayer, player=3 - self.player, parent=self, move=action)  # si on met simplement self.board, l'enfant et le parent auront tous les 2 la même array avec 2 références différentes. La modification dans un des objets impactera l'array et cette modification sera visible dans l'autre objet
+        childNode.board[action] = childNode.player
         self.childArray.append(childNode)
 
     def simulate(self):
-        tempoNode = Node(deepcopy(self.board), player=3-self.player, move=self.move)
+        if self.checkStatus() != -1 and self.checkStatus() != self.rootPlayer:  # l'adversaire gagne avec cette board
+            self.parent.numberOfWins = -1000000  # le move précédent du joueur a conduit à cette configuration. Inutile d'explorer les autres enfants car la configuartion est trop dangeureusue
+        tempoNode = Node(deepcopy(self.board), rootPlayer=self.rootPlayer, player=3 - self.player, move=self.move)
         while tempoNode.checkStatus() == -1:
             tempoNode.randomMove()
         return tempoNode.checkStatus()
 
     def backPropagate(self, winner):
         self.numberOfVisits += 1
-        if winner == self.player:
+        if winner == self.rootPlayer:
             self.numberOfWins += 1
         if self.parent:
             self.parent.backPropagate(winner)
@@ -91,10 +95,9 @@ class Node:
         return 0  # égalité
 
 
-def bestMove(board, player):
+def bestMove(board, player, time):
     c = np.sqrt(2)
-    root = Node(board, player=player)
-    time = 100
+    root = Node(board, rootPlayer=player, player=3-player)
 
     for t in range(time):
         node = root
@@ -114,17 +117,64 @@ def bestMove(board, player):
 
 
 if __name__ == '__main__':
-    board = [0] * 42
-    currentPlayer = 2
+    timeVector = np.array([10, 20, 15, 17, 19])
+    nbrRuns = 100
+    bilan = np.zeros(nbrRuns, dtype=int)
+    bilanWin1 = np.zeros(len(timeVector), dtype=int)
+    bilanWin2 = np.zeros(len(timeVector), dtype=int)
+    bilanTie = np.zeros(len(timeVector), dtype=int)
+    bilanVide = np.zeros(nbrRuns, dtype=int)
+    bilanCasesVides = np.zeros(len(timeVector), dtype=float)
 
-    for i in range(41):  # 2 IA s'affrontent
-        bestNode = bestMove(board, currentPlayer)
-        board = bestNode.board
-        currentPlayer = 3 - currentPlayer
-        print(bestNode.move)
-        if bestNode.checkStatus() != -1:
-            break
+    currentPlayer = 1
 
-    for i in range(5, -1, -1):
-        print(board[0 + 7 * i: 7 + 7 * i])
-    print("winner is : " + str(bestNode.checkStatus()) + ", winner move : " + str(bestNode.move))
+    i = 0
+    for time in range(len(timeVector)):
+        print(timeVector[i])
+        for j in range(nbrRuns):
+            print("Run : " + str(j))
+            board = [0] * 42
+            for k in range(41):  # 2 IA s'affrontent
+                bestNode = bestMove(board, currentPlayer, timeVector[i])
+                board = bestNode.board
+                currentPlayer = 3 - currentPlayer
+                #print(bestNode.move)
+                if bestNode.checkStatus() != -1:
+                    break
+            bilan[j] = bestNode.checkStatus()
+            bilanVide[j] = board.count(0)
+
+        bilanWin1[i] = np.count_nonzero(bilan == 1)
+        bilanWin2[i] = np.count_nonzero(bilan == 2)
+        bilanTie[i] = np.count_nonzero(bilan == -1)
+        bilanCasesVides[i] = np.mean(bilanVide)/42
+
+        bilan = np.zeros(nbrRuns)
+        bilanVide = np.zeros(nbrRuns)
+        i += 1
+
+        #print(f'Pourcentage de cases vides : {np.mean(bilanCasesVides)/42}')
+        #print(f'{nbrRuns} Runs à {time} itérations - victoires de 1 : {bilan.count(1)}, victoires de 2 : {bilan.count(2)}, égalités : {bilan.count(-1)}')
+
+    print(bilanWin1, bilanWin2, bilanTie, bilanCasesVides)
+
+    default_x_ticks = range(len(timeVector))
+    plt.bar(default_x_ticks, bilanWin1, color='r', width=0.2)
+    plt.bar(default_x_ticks, bilanWin2, bottom=bilanWin1, color='b', width=0.2)
+    plt.bar(default_x_ticks, bilanTie, bottom=bilanWin1 + bilanWin2, color='g', width=0.2)
+
+    plt.xlabel("Nombre d'explorations de l'arbre à partir du nœud root")
+    plt.legend(["Victoires de 1", "Victoires de 2", "Égalités"])
+    plt.title(f'Résultats de {nbrRuns} parties en fonction du nombre d\'explorations')
+    plt.xticks(default_x_ticks, timeVector)
+    plt.show()
+
+    plt.bar(default_x_ticks, bilanCasesVides, color='black', width=0.2)
+    plt.xlabel("Nombre d'explorations de l'arbre à partir du nœud root")
+    plt.title(f'Pourcentage moyen du nombre de cases vides à la fin de {nbrRuns} parties')
+    plt.xticks(default_x_ticks, timeVector)
+    plt.show()
+
+    #for i in range(5, -1, -1):
+    #    print(board[0 + 7 * i: 7 + 7 * i])
+    #print("winner is : " + str(bestNode.checkStatus()) + ", last move : " + str(bestNode.move))
